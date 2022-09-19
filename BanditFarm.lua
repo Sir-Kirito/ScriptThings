5837c98aeb611078616eb58295f2c0b32694a230
Kirito <70341302+Sir-Kirito@users.noreply.github.com>
2022-09-19 15:45:58 +0300

    Update BanditFarm.lua

--- BanditFarm.lua
+++ BanditFarm.lua
@@ -2,8 +2,7 @@ while _G.BanditFarm do
     wait(.1)
 for _, instance in next, game:GetService("Workspace")["___Game"]["__Render"]["Island_Starter"]["__Npcs"]["___Mobs"]:GetChildren() do
     if instance.Name == "Bandit" and instance:WaitForChild("Humanoid").Health >0 then
-       local behindamount = -3.5
-    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = instance:WaitForChild("HumanoidRootPart").CFrame + instance:WaitForChild("HumanoidRootPart").CFrame.lookVector * behindamount
+    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = instance:WaitForChild("HumanoidRootPart").CFrame + instance:WaitForChild("HumanoidRootPart").CFrame.lookVector * distance
 end
 end
 end
