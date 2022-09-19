ba5abf5d48e31a4ae00a8f9017ea4cfc737d4584
Kirito <70341302+Sir-Kirito@users.noreply.github.com>
2022-09-19 16:09:28 +0300

    Update BanditFarm.lua

--- BanditFarm.lua
+++ BanditFarm.lua
@@ -1,6 +1,5 @@
 while _G.BanditFarm do
     wait(.1)
-            local distance = FarmDistance
 for _, instance in next, game:GetService("Workspace")["___Game"]["__Render"]["Island_Starter"]["__Npcs"]["___Mobs"]:GetChildren() do
     if instance.Name == "Bandit" and instance:WaitForChild("Humanoid").Health >0 then
     game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = instance:WaitForChild("HumanoidRootPart").CFrame + instance:WaitForChild("HumanoidRootPart").CFrame.lookVector * distance
